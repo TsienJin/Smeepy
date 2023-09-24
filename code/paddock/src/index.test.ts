2cf47ea2395b6da2b2c43fa1716305f6d86481ed
Tsien Jin <46650957+TsienJin@users.noreply.github.com>
2023-09-24 15:11:10 +0800

    INitial commit with testing framework

---
+++ code/paddock/src/index.test.ts
@@ -0,0 +1,8 @@
+import {describe, expect, test} from "@jest/globals";
+
+
+describe('Addition', ()=>{
+  test('1+2', ()=>{
+    expect(1+2).toBe(3)
+  })
+})
--- code/paddock/src/index.ts
+++ code/paddock/src/index.ts
@@ -1,3 +1,29 @@
+import express, {Request, Response} from "express"
+import * as dotenv from "dotenv"
+import * as process from "process"
 
 
-console.log("val, something")
+/**
+ * Loads .env.dev only if in dev environment.
+ *
+ * Possible Environments:
+ * 1. dev
+ * 2. staging (handled by docker)
+ * 3. production (handled by docker)
+ */
+if(process.env.NODE_ENV==="dev"){
+  console.info("Loading development environment!")
+  dotenv.config({
+    path: __dirname+"/../.env.dev"
+  })
+}
+
+const app = express()
+
+app.get("/", (req:Request, res:Response) => {
+  res.send("Hello world!")
+})
+
+app.listen(process.env.EXPRESS_PORT, ()=>{
+  console.info(`🚀 Express app listening on port ${process.env.EXPRESS_PORT}`)
+})
