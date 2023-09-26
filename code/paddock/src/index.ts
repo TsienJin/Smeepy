32fa2b8bcb7c13352067fddc7ad130cd96807809
Tsien Jin <tsien.jin@gmail.com>
2023-09-26 22:30:00 +0800

    playing around with redis

--- code/paddock/src/index.ts
+++ code/paddock/src/index.ts
@@ -1,14 +1,26 @@
 import express, {Request, Response} from "express"
 import * as process from "process"
-
+import {createClient} from "redis";
 
 const app = express()
+
+const redis = createClient({
+  url: process.env.DATABASE_REDIS
+})
+
+
+
 
 app.get("/", (req:Request, res:Response) => {
   res.send("Hello world!")
 })
 
-app.listen(process.env.EXPRESS_PORT||8000, ()=>{
+app.listen(process.env.EXPRESS_PORT||8000, async ()=>{
   if (process.env.EXPRESS_PORT==undefined){console.warn("Express Port not set! Using fallback port!")}
   console.info(`🚀 Express app listening on port ${process.env.EXPRESS_PORT||8000}`)
+
+  await redis.connect()
+  console.log(await redis.subscribe('key', message => {
+    console.log(message)
+  }))
 })
