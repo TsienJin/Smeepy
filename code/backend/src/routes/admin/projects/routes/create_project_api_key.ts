1ecc9e0425cf15398b8ecccc1f2a98afaf95a025
Tsien Jin <46650957+TsienJin@users.noreply.github.com>
2023-10-10 01:40:50 +0800

    Added endpoint to create API key

---
+++ code/backend/src/routes/admin/projects/routes/create_project_api_key.ts
@@ -0,0 +1,43 @@
+import {Handler, Request, Response} from "express";
+import {decodeJwt} from "@src/utils/jwt";
+import {validate_schema} from "@src/schema/validate_schema";
+import {
+  endpoint_project_create_api_key_schema,
+  endpoint_project_create_api_key_schema_validator
+} from "@routes/admin/projects/structs/project_endpoints";
+import DBClient from "@src/objects/db";
+
+
+export const create_project_api_key:Handler = async(req:Request, res:Response)=> {
+
+  try{
+
+    const params = await validate_schema<endpoint_project_create_api_key_schema>(endpoint_project_create_api_key_schema_validator, req.body)
+
+    if(params===undefined){
+      res.status(400).json({message:"Missing parameters!"})
+      return
+    }
+    
+    const api_key = await DBClient.instance.paddock_api_key.create({
+      data: {
+        label: params.api_label,
+        description: params.api_description,
+        paddock_project: {
+          connect: {
+            id: params.project_id
+          }
+        }
+      },
+
+    })
+
+    res.status(200).json({message:"Ok!", api_key:api_key})
+
+
+  } catch (e) {
+    console.error(e)
+    res.status(500).json({message:"Unable to create API key for this project!"})
+  }
+
+}
