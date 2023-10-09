d73e32341fcff4c56b89903c70518f85f1c93f99
Tsien Jin <46650957+TsienJin@users.noreply.github.com>
2023-10-10 01:39:25 +0800

    Added user login route

--- code/backend/src/routes/admin/user/user_router.ts
+++ code/backend/src/routes/admin/user/user_router.ts
@@ -1,24 +1,19 @@
 import express, {Handler, Request, Response} from "express";
-import {endpoint_user_create_schema, endpoint_user_create_schema_validator} from "@src/schema/endpoints/user";
-import DBClient from "@src/objects/db";
-import bcrypt from 'bcrypt'
-import * as console from "console";
-import {generateJwt} from "@src/utils/jwt";
-import {set_auth_header} from "@src/utils/auth_header";
-import {validate_schema} from "@src/schema/validate_schema";
 import {create_user} from "@routes/admin/user/routes/create";
+import {login_user} from "@routes/admin/user/routes/login";
 
 
 export const user_router = express.Router()
 user_router.use(express.json())
-
-
-const something:Handler = async (req:Request, res:Response) => {
-
-}
+user_router.use(express.raw())
 
 
 /**
  * Endpoint to create a new user
  */
 user_router.post("/create", create_user)
+
+/**
+ * Endpoint to login user using email password
+ */
+user_router.post("/login", login_user)
