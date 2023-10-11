4c129225bdbb953bd498b249ecf83771e8767dd5
Tsien Jin <tsien.jin@gmail.com>
2023-10-12 01:05:45 +0800

    Method to set user JWT

--- code/backend/src/middleware/authorisation/require_login.ts
+++ code/backend/src/middleware/authorisation/require_login.ts
@@ -1,14 +1,18 @@
 import {Handler, NextFunction, Request, Response} from "express";
 import {decodeJwt, generateJwt} from "@src/utils/jwt";
+import {BackendHandler, BackendRequest} from "@src/globals/types_and_all";
 
 
-export default function require_login():Handler{
-  return async(req:Request, res:Response, next:NextFunction) => {
+export default function require_login():BackendHandler{
+  return async(req:BackendRequest, res:Response, next:NextFunction) => {
     try{
       
       // fetch and decode current JWT
       const token = req.headers.authorization
       const usrJwt = decodeJwt(token||"")
+
+      // sets JWT attr in current req
+      req.jwt = usrJwt
 
       // regenerate JWT to 'reset' the expiry on JWT token
       const newJwt = generateJwt({user_id:usrJwt.user_id})
