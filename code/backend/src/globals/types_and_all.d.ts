530fe48b9a1e2273d09a334ee79518c7586aeed1
Tsien Jin <tsien.jin@gmail.com>
2023-10-12 01:06:13 +0800

    Added defiitions for extedned backend and req

---
+++ code/backend/src/globals/types_and_all.d.ts
@@ -0,0 +1,31 @@
+import {Handler, Request} from "express";
+import {IJwt} from "@src/utils/jwt.types";
+import {NextFunction, Response} from "express-serve-static-core";
+
+
+export enum API_SERVICES {
+  Beaver="Beaver",
+}
+
+
+/**
+ * Backend Request object incorporating JWT items and API key to be easily accessible from the backend.
+ */
+export interface BackendRequest extends Request {
+  jwt?: IJwt,
+  api?: string
+}
+
+
+/**
+ * Updated types to include Backend Request
+ */
+export interface BackendHandler extends Handler {
+  (req: BackendRequest<P, ResBody, ReqBody, ReqQuery, LocalsObj>,
+   res: Response<ResBody, LocalsObj>,
+   next: NextFunction)
+  :void
+}
+
+
+
