2a4d0b0050cfe6042c338d3e46643833dd01e0cc
Tsien Jin <tsien.jin@gmail.com>
2023-10-12 15:48:01 +0800

    Changed signature of method call

--- code/backend/src/utils/auth_header.ts
+++ code/backend/src/utils/auth_header.ts
@@ -11,6 +11,6 @@ export function set_auth_header(jwt_string:string): string {
  * Method to remove "Bearer " from authorization header string
  * @param auth_header
  */
-export function extract_jwt_from_auth_header_string(auth_header:string): string {
+export function extract_val_from_auth_header_string(auth_header:string): string {
   return auth_header.replace("Bearer ","")
 }
