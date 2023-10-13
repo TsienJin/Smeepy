ea3371595953e303f0c8d959b15be8d67013820c
Tsien Jin <tsien.jin@gmail.com>
2023-10-13 23:26:07 +0800

    Updated IApi from prisma

--- code/backend/src/globals/types_and_all.types.ts
+++ code/backend/src/globals/types_and_all.types.ts
@@ -1,11 +1,23 @@
 import {Handler, Request} from "express";
 import {IJwt} from "@src/utils/jwt.types";
 import {NextFunction, Response} from "express-serve-static-core";
+import {Prisma} from "@prisma/client";
 
 
+/**
+ * ENUM for available services for API products
+ */
 export enum API_SERVICES {
   Beaver="Beaver",
 }
+
+
+/**
+ * Type for IApi
+ * Reference: https://www.prisma.io/docs/concepts/components/prisma-client/advanced-type-safety/operating-against-partial-structures-of-model-types
+ */
+const __api_with_services = Prisma.validator<Prisma.paddock_api_keyDefaultArgs>()({include:{paddock_api_key_services:true}})
+export type IApi = Prisma.paddock_api_keyGetPayload<typeof __api_with_services>
 
 
 /**
@@ -13,7 +25,7 @@ export enum API_SERVICES {
  */
 export interface BackendRequest extends Request {
   jwt?: IJwt,
-  api?: string
+  api?: IApi
 }
 
 
