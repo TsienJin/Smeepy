763a059b30a303b8ce6c1dad77a3ebacfa496521
Tsien Jin <tsien.jin@gmail.com>
2023-10-13 23:27:47 +0800

    Endpoint for creating beaver log

---
+++ code/backend/src/routes/api/beaver/routes/create_log.ts
@@ -0,0 +1,44 @@
+import {BackendHandler, BackendRequest, IApi} from "@src/globals/types_and_all.types";
+import {Response} from "express";
+import assert_not_null from "@src/utils/assert_not_null";
+import {validate_schema} from "@src/schema/validate_schema";
+import {
+  endpoint_beaver_log_create_schema,
+  endpoint_beaver_log_create_schema_validator
+} from "@routes/api/beaver/structs/create_log_struct";
+import DBClient from "@src/objects/db";
+
+
+/**
+ * Endpoint to receive log
+ * @param req {BackendRequest}
+ * @param res {Response}
+ */
+export const beaver_create_log:BackendHandler = async (req:BackendRequest, res:Response) => {
+
+  try {
+
+    const api_info = assert_not_null<IApi>(req.api)
+    const data = await validate_schema<endpoint_beaver_log_create_schema>(endpoint_beaver_log_create_schema_validator, req.body)
+    const solid_data = assert_not_null<endpoint_beaver_log_create_schema>(data)
+
+    const newLog = await DBClient.instance.beaver_log.create({
+      data: {
+        label: solid_data.label,
+        log: solid_data.log,
+        level: solid_data.level||"INFO",
+        paddock_api_id: api_info.id
+      }
+    })
+
+    console.log(newLog)
+
+
+    res.send("OK!")
+
+  } catch (e) {
+    console.error(e)
+    res.status(500).json({message: "An oopsie occurred while saving your log!"})
+  }
+
+}
