7c263b5d3639f867ac2c1d331c009c548e672056
Tsien Jin <tsien.jin@gmail.com>
2023-10-13 23:26:26 +0800

    Added create log endpoint

--- code/backend/src/routes/api/beaver/beaver_router.ts
+++ code/backend/src/routes/api/beaver/beaver_router.ts
@@ -1,14 +1,27 @@
 import express, {Request, Response} from "express";
 import {validate_api_key} from "@middleware/authentication/validate_api_key";
 import {API_SERVICES} from "@src/globals/types_and_all.types";
+import {beaver_create_log} from "@routes/api/beaver/routes/create_log";
 
 
 export const beaver_router = express.Router()
+beaver_router.use(validate_api_key(API_SERVICES.Beaver))
+beaver_router.use(express.json())
+beaver_router.use(express.raw())
 
 
 /**
  * Testing the endpoint
  */
-beaver_router.get("/", validate_api_key(API_SERVICES.Beaver), (req:Request, res:Response)=>{
+beaver_router.get("/",(req:Request, res:Response)=>{
   res.send("Beaver")
 })
+
+
+/**
+ * Endpoint to CREATE LOG
+ */
+beaver_router.post("/log", beaver_create_log)
+
+
+
