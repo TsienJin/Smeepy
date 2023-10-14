65c0648047e368ecb4826efa10a51c84ffbba743
Tsien Jin <tsien.jin@gmail.com>
2023-10-15 02:03:46 +0800

    Stuff playing around with ws

--- code/backend/src/index.ts
+++ code/backend/src/index.ts
@@ -1,14 +1,19 @@
 import express, {Request, Response} from "express"
 import * as process from "process"
-import {createClient} from "redis";
 import {admin_router} from "@routes/admin/admin_router";
 import {api_router} from "@routes/api/api_router";
+import {createServer} from "http";
+import {WebSocketServer} from "ws";
 
 const app = express()
 
-// const redis = createClient({
-//   url: process.env.DATABASE_REDIS
-// })
+app.listen(process.env.EXPRESS_PORT||8000, ()=>{
+  if (process.env.EXPRESS_PORT==undefined){console.warn("Express Port not set! Using fallback port!")}
+  console.info(`🚀 Express app listening on port ${process.env.EXPRESS_PORT||8000}`)
+})
+
+
+
 
 
 
@@ -22,13 +27,3 @@ app.use("/admin", admin_router)
 
 // api router for client facing endpoints
 app.use("/api", api_router)
-
-app.listen(process.env.EXPRESS_PORT||8000, ()=>{
-  if (process.env.EXPRESS_PORT==undefined){console.warn("Express Port not set! Using fallback port!")}
-  console.info(`🚀 Express app listening on port ${process.env.EXPRESS_PORT||8000}`)
-  //
-  // await redis.connect()
-  // console.log(await redis.subscribe('key', message => {
-  //   console.log(message)
-  // }))
-})
