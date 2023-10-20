e38ea311833c914437f9edb1e53d7a863c165553
Tsien Jin <46650957+TsienJin@users.noreply.github.com>
2023-10-20 15:01:47 +0800

    Updated signature

--- code/backend/src/utils/assert_not_null.ts
+++ code/backend/src/utils/assert_not_null.ts
@@ -2,7 +2,7 @@
  * Method to ensure that val is defined
  * @param val
  */
-export default function assert_not_null<T>(val:T|undefined|null):T{
+export default function assert_not_null<T>(val:T|undefined|null|any):T{
   if(val===undefined||val===null){
     throw new Error("Assert_not_null error!")
   }
