556c4decc4ef93eda37396a5167a0b281cfadb70
Tsien Jin <46650957+TsienJin@users.noreply.github.com>
2023-10-20 14:51:30 +0800

    updated environemnt schema

--- code/paddock/src/env.d.ts
+++ code/paddock/src/env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  readonly PUBLIC_BACKEND_URL: string;
+  readonly PUBLIC_PADDOCK_URL: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
