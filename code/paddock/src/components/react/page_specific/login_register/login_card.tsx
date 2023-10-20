910c10739fc7de86ccdcc6df5d8bf1aedcf1e39e
Tsien Jin <46650957+TsienJin@users.noreply.github.com>
2023-10-20 14:51:52 +0800

    Added login card

--- code/paddock/src/components/react/page_specific/login_register/login_card.tsx
+++ code/paddock/src/components/react/page_specific/login_register/login_card.tsx
@@ -4,18 +4,36 @@ import {ReactWireCard} from "../../primatives/wrapper/Wire_Card.tsx";
 import {ReactInputField} from "../../primatives/input/ReactInputField.tsx";
 import {ReactFormWrapper} from "../../primatives/wrapper/FormWrapper.tsx";
 import {ReactActionButton} from "../../primatives/click/ActionButton.tsx";
-import {useState} from "react";
+import {useEffect, useState} from "react";
+import axios from "axios";
+import {handle_login} from "../../../../functions/auth/handle_login.ts";
+import replaceLocal from "../../../../functions/util/replace_local.ts";
+import {useLocalStorage} from "usehooks-ts";
 
 
 export const ReactLoginCard = () => {
 
+  const [smeepyToken, setSmeepyToken] = useLocalStorage("smeepy", "")
 
   const [waitingRes, setWaitingRes] = useState<boolean>(false)
 
+  const [email, setEmail] = useState<string>("")
+  const [password, setPassword] = useState<string>("")
+
 
   const submitForm = () => {
-    console.log("Submitting!")
-    setWaitingRes(true)
+    if(email && password){
+      setWaitingRes(true)
+      handle_login(email, password)
+        .then(r=>{
+          console.log(r)
+          if(r){
+            setSmeepyToken(r)
+            replaceLocal("/dashboard")
+          }
+        })
+        .then(()=>{setWaitingRes(false)})
+    }
   }
 
 
@@ -30,8 +48,8 @@ export const ReactLoginCard = () => {
           <ReactQuestionLink question={"Don't have an account?"} link={"/signup"} linkText={"Sign up"} target={"_self"} />
         </div>
         <ReactFormWrapper action={submitForm}>
-          <ReactInputField label={"Email"} />
-          <ReactInputField label={"Password"} />
+          <ReactInputField label={"Email"} hoist={setEmail} />
+          <ReactInputField label={"Password"} hoist={setPassword} type={"password"}/>
           <ReactActionButton text={"Login"} type={"submit"} action={submitForm} loading={waitingRes}/>
         </ReactFormWrapper>
       </div>
