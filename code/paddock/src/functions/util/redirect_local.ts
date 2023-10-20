beba5487163a899d6bc0a42d33d6a78a57c310a7
Tsien Jin <46650957+TsienJin@users.noreply.github.com>
2023-10-20 15:38:20 +0800

    changed redirect functionality

--- code/paddock/src/functions/util/redirect_local.ts
+++ code/paddock/src/functions/util/redirect_local.ts
@@ -3,5 +3,5 @@
 
 
 export default function redirectLocal(path:string):void{
-  window.location.replace(`${import.meta.env.PUBLIC_PADDOCK_URL}${path}`)
+  window.location.href = `${import.meta.env.PUBLIC_PADDOCK_URL}${path}`
 }
