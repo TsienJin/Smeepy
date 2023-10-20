97de39a64bc7a2cf559ad27764fafb27e849c5f7
Tsien Jin <46650957+TsienJin@users.noreply.github.com>
2023-10-20 14:52:03 +0800

    Redirect to dashboard if user is authed

---
+++ code/paddock/src/components/react/non_visual/dashboard_if_logged_in.tsx
@@ -0,0 +1,22 @@
+import {useLocalStorage} from "usehooks-ts";
+import axios from "axios";
+import replaceLocal from "../../../functions/util/replace_local.ts";
+
+
+export const Dashboard_if_logged_in = () => {
+
+  const [token, setToken] = useLocalStorage("smeepy", "")
+
+  if(token){
+    axios.get(`${import.meta.env.PUBLIC_BACKEND_URL}/admin/user/validate`, {
+      headers:{
+        smeepy: token
+      }
+    })
+      .then(()=>{replaceLocal("/dashboard")})
+      .catch(()=>{setToken("")})
+  }
+
+
+  return(<></>)
+}
