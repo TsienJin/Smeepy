151476543c695fdd7a1e6860e919ab6a177973ab
Tsien Jin <46650957+TsienJin@users.noreply.github.com>
2023-10-20 15:05:37 +0800

    Added headers to allow smeepy

--- code/backend/src/middleware/authorisation/cors.ts
+++ code/backend/src/middleware/authorisation/cors.ts
@@ -12,6 +12,11 @@ export default function cors(...origins:string[]){
 
     if(origin!=undefined && !origins.includes("") && origins.includes(origin)){
       res.setHeader("X-Frame-Options", `DENY`)
+      res.setHeader("Access-Control-Allow-Origin", `${process.env.PADDOCK_HOST}`)
+      res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
+      res.setHeader('Access-Control-Allow-Headers', 'Content-Type, smeepy')
+      res.setHeader('Access-Control-Expose-Headers', 'smeepy')
+      res.setHeader('Access-Control-Allow-Credentials', 'true')
       next()
     } else {
       res.status(400).json({error:"CORS violation error"})
