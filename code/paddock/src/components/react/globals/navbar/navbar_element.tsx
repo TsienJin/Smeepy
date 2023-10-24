dcb8723d6f99c98e146bde8864ef81c32fd140fc
Tsien Jin <46650957+TsienJin@users.noreply.github.com>
2023-10-25 02:14:41 +0800

    Added navbar elements

---
+++ code/paddock/src/components/react/globals/navbar/navbar_element.tsx
@@ -0,0 +1,39 @@
+import {useState} from "react";
+
+
+export const ReactNavbarElement = (
+  {
+    label,
+    link,
+    children,
+    expanded=true,
+    callback=()=>{}
+  }:{
+    label:string,
+    link:string,
+    children?:any,
+    expanded?:boolean,
+    callback?:any
+  }
+) => {
+
+  const handleClick = () => {
+    callback()
+  }
+
+  return(
+    <a href={link} target={"_self"} onClick={handleClick} className={`w-full`}>
+      <div className={`
+      transition-all
+      flex flex-row p-[10px] rounded bg-shadow-900 md:hover:bg-shadow-800 text-shadow-400 w-full
+      `}>
+        <div className={`flex flex-row justify-start items-center text-center w-full`}>
+          {children}
+          <div className={`overflow-hidden text-sm font-normal flex flex-row justify-center items-start ${expanded?"":"max-w-0"}`}>
+            <span className={`pl-2 ml-2 border-l border-shadow-400 h-full`}>{label}</span>
+          </div>
+        </div>
+      </div>
+    </a>
+  )
+}
