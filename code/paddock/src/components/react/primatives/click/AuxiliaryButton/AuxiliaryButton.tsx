e73eff3f09476b677087b6cfc60bcfa76ec1b2a4
Tsien Jin <46650957+TsienJin@users.noreply.github.com>
2023-12-03 19:08:52 +0800

    Added auxiliary buttons

---
+++ code/paddock/src/components/react/primatives/click/AuxiliaryButton/AuxiliaryButton.tsx
@@ -0,0 +1,42 @@
+import {twMerge} from "tailwind-merge";
+import {useEffect} from "react";
+
+
+export const AuxiliaryButton = (
+  {
+    icon="",
+    children="",
+    className="",
+    outerClassName="",
+    action=()=>{}
+  }:{
+    icon?:any,
+    children?:any,
+    className?:string,
+    outerClassName?:string,
+    action?:any,
+  }
+) => {
+
+  const handleClick = () => {
+    action()
+    console.log(`[${children}] Button Pressed!`)
+  }
+
+
+  return(
+    <button onClick={handleClick} className={twMerge(`w-fit`, outerClassName)}>
+      <div className={twMerge(`
+      flex flex-row justify-center items-center 
+      gap-x-2 px-3 py-1 
+      transition-colors
+      bg-shadow-100 md:hover:bg-shadow-200 text-shadow-700 whitespace-nowrap
+      rounded
+      border border-shadow-300
+      `, className)}>
+        {icon}
+        {children}
+      </div>
+    </button>
+  )
+}
