9f1dc1c84875a89ff3a270e7f7f79427f0483c04
Tsien Jin <tsien.jin@gmail.com>
2023-12-24 01:15:38 +0800

    Added basic tab section

---
+++ code/paddock/src/components/react/primatives/tabs/tab_section.tsx
@@ -0,0 +1,25 @@
+
+
+
+
+export const TabSection = (
+  {
+    label="",
+    current="",
+    children=""
+  }:{
+    label:string,
+    current?:string,
+    children?:any
+  }
+) => {
+
+  return(
+    <section className={`
+    w-full
+    ${current===label?"":"opacity-0 h-0"}
+    `}>
+      <span>{label}</span>
+    </section>
+  )
+}
