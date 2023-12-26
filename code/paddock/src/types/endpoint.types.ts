9ee9bc9649fbeed4400db13221873418db1e18ff
Tsien Jin <46650957+TsienJin@users.noreply.github.com>
2023-12-26 14:20:27 +0800

    Updated endpoint for API credentials

--- code/paddock/src/types/endpoint.types.ts
+++ code/paddock/src/types/endpoint.types.ts
@@ -1,11 +1,17 @@
 /**
  * This file contains the schema for the response from the various backend endpoints
  */
-import type {Project} from "./object.types.ts";
+import type {ApiKeyCredentials, Project} from "./object.types.ts";
 
 
 // URL/admin/project/get-by-id
 export type Project_get_by_id = {
   message: string,
   product: Project
+}
+
+// URL/admin/project/create-api-key
+export type Create_api_key = {
+  message: string,
+  api_key: ApiKeyCredentials
 }
