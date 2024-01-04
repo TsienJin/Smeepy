3826266bd5ffc2b43a78727610527c10acffea74
Tsien Jin <tsien.jin@gmail.com>
2024-01-04 22:35:01 +0800

    Improvements to refreshing API keyts

--- code/paddock/src/components/react/page_specific/projects/project/api_keys/APIKeysTab.tsx
+++ code/paddock/src/components/react/page_specific/projects/project/api_keys/APIKeysTab.tsx
@@ -2,9 +2,13 @@ import {IslandTitleGroup} from "../dashboard/islands/IslandTitleGroup.tsx";
 import {AuxiliaryButtonContainer} from "../../../../primatives/click/AuxiliaryButton/AuxilaryButtonContainer.tsx";
 import {AuxiliaryButton} from "../../../../primatives/click/AuxiliaryButton/AuxiliaryButton.tsx";
 import {ArrowDownAZIcon, KeyRound, RefreshCwIcon, SlidersHorizontal} from "lucide-react";
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import {ModalWindow} from "../../../../modal/ModalWindow.tsx";
 import {NewAPIKeyModal} from "./NewAPIKeyModal.tsx";
+import type {ApiKey} from "../../../../../../types/object.types.ts";
+import get_project_api_keys from "../../../../../../functions/projects/get_project_api_keys.ts";
+import useLocalStorageHook from "../../../../../../util/react/localstoragehook.ts";
+import {APIKeysTable} from "./APIKeysTable.tsx";
 
 /**
  * Component responsible for rendering the table with API keys and products etc.
@@ -25,6 +29,11 @@ export const APIKeysTab = (
   const [modalChild, setModalChild] = useState<any>(undefined)
   const [modalCloseSem, setModalCloseSem] = useState<number|string>("")
 
+  const [smeepyToken, setSmeepyToken] = useLocalStorageHook("smeepy")
+  const [apiKeys, setApiKeys] = useState<ApiKey[]>([])
+
+  const [refreshingList, setRefreshingList] = useState<boolean>(false)
+
   const handleClose = () => {
     updateDashboard()
     setTimeout(()=>{
@@ -34,12 +43,27 @@ export const APIKeysTab = (
 
   const onDone = () => {
     setModalCloseSem(Math.random()*100000)
+    fetchApiKeys()
     handleClose()
+    updateDashboard()
   }
 
   const handleCreateAPIKey = () => {
     setModalChild(<NewAPIKeyModal id={id} onDone={onDone}/>)
   }
+
+  const fetchApiKeys = () => {
+    setRefreshingList(true)
+    updateDashboard()
+    get_project_api_keys(id, smeepyToken)
+      .then(setApiKeys)
+      .then(()=>{setRefreshingList(false)})
+      .catch(console.error)
+  }
+
+  useEffect(() => {
+    fetchApiKeys()
+  }, []);
 
   return(
     <>
